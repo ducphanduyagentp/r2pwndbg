50774cf664cd0f3909706a590f823d6c3588f913
Duc Phan <ducphanduyagentp@users.noreply.github.com>
2018-12-29 09:07:05 -0500

    More specific stubs

--- r2pwndbg/r2pwndbg.py
+++ r2pwndbg/r2pwndbg.py
@@ -13,6 +13,7 @@ known_cmds = {
     "step": generate_known_cmd("step"),
     "info": generate_known_cmd("info"),
     "heap": generate_known_cmd("heap"),
+    "disassemble": generate_known_cmd("disassemble"),
     "context": ["context"],
     "exit": ["exit"],
     "quit": generate_known_cmd("quit")
@@ -29,12 +30,35 @@ class r2pwndbg:
         if filename != None:
             self.r2 = r2pipe.open(filename, flags=['-d', '-2'])
 
+    def r2file(self, args):
+        pass
+
+    def r2continue(self, args):
+        pass
+
+    def r2step(self, args):
+        pass
+    
+    def r2info(self, args):
+        pass
+    
+    def r2heap(self, args):
+        pass
+
+    def r2context(self, args):
+        pass
+    
+    def r2quit(self):
+        self.r2.quit()
+        exit(0)
 
 def main():
 
     if len(sys.argv) > 1:
         filename = sys.argv[1]
         r2 = r2pwndbg(filename)
+    else:
+        r2 = None
 
     while True:
         cmd = input(p_red("r2pwndbg> ")).strip()
@@ -51,21 +75,32 @@ def main():
 
         if is_known(cmd, "file"):
             print("file command")
+            continue
+        elif is_known(cmd, "exit") or is_known(cmd, "quit"):
+            if r2 != None:
+                r2.r2quit()
+            else:
+                exit(0)
         elif is_known(cmd, "continue"):
+            assert r2 != None, "No file loaded"
             print("continue command")
         elif is_known(cmd, "step"):
+            assert r2 != None, "No file loaded"
             print("step command")
         elif is_known(cmd, "info"):
+            assert r2 != None, "No file loaded"
             print("info command")
         elif is_known(cmd, "heap"):
+            assert r2 != None, "No file loaded"
             print("heap command")
         elif is_known(cmd, "context"):
+            assert r2 != None, "No file loaded"
             print("context command")
-        elif is_known(cmd, "exit") or is_known(cmd, "quit"):
-            exit(0)
+        elif is_known(cmd, "disassemble"):
+            assert r2 != None, "No file loaded"
+            print("disassemble command")
         else:
             print("unknown command")
-
 
 
 if __name__ == '__main__':
